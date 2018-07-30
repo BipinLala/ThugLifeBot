0cc4560fa0da3a6763cf390063a20e62999de18a
BipinLala <lala.bipin@gmail.com>
2018-07-31 02:34:04 +0530

    gifs added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 3000;
 
 
 var oxford = require('project-oxford');
-var client = new oxford.Client('PROJECT OXFORD KEY', 'westcentralus');
+var client = new oxford.Client('a668de7d85634222a6f40589220a0cbf', 'westcentralus');
 
 server.listen(port, function(){
     console.log("Server Running on port 3000");
